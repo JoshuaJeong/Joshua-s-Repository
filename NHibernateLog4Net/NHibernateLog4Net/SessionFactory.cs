afafeb18f8b48919ea45db11a190b08ae9bc0fa8
unknown <jwjeong12@hanmail.net>
2017-12-17 23:52:24 +0900

    NHibernate Logging added

---
+++ NHibernateLog4Net/NHibernateLog4Net/SessionFactory.cs
@@ -0,0 +1,39 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernateLog4Net
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    string connString = System.Configuration.ConfigurationManager.ConnectionStrings["log"].ConnectionString;
+
+                    _sessionFactory = Fluently.Configure()
+                    .Database(MsSqlConfiguration.MsSql2005
+                    .ConnectionString(System.Configuration.ConfigurationManager.ConnectionStrings["log"].ConnectionString))
+										.Mappings(m => m.FluentMappings.AddFromAssemblyOf<StoreMap>())
+                    .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ProductMap>())
+                    .ExposeConfiguration(config =>
+                    {
+                        SchemaExport schemaExport = new SchemaExport(config);
+                    })
+                    .BuildSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
---
+++ NHibernateLog4Net/NHibernateLog4Net/Tables/log.cs
@@ -0,0 +1,35 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+
+namespace NHibernateLog4Net
+{
+    public class Log
+    {
+        public virtual int Id { get; set; }
+        public virtual string ApplicationEntity { get; set; }
+        public virtual string Method { get; set; }
+        public virtual string RequestMessage { get; set; }
+        public virtual string ResponseMessage { get; set; }
+        public virtual string UserMessage { get; set; }
+        public virtual string Endpoint { get; set; }
+        public virtual DateTime Regdate { get; set; }
+        public virtual string RequesterIPAddress { get; set; }
+    }
+
+    public class LogMap : ClassMap<Log>
+    {
+        public LogMap()
+        {
+            Id(u => u.Id);
+            Map(u => u.ApplicationEntity).Nullable();
+            Map(u => u.Method).Nullable();
+            Map(u => u.RequestMessage).Nullable();
+            Map(u => u.ResponseMessage).Nullable();
+            Map(u => u.UserMessage).Nullable();
+            Map(u => u.Endpoint).Nullable();
+            Map(u => u.Regdate).Nullable();
+            Map(u => u.RequesterIPAddress).Nullable();
+            Table("TB_LOG");
+        }
+    }
+}

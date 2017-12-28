432a5c93a3274743986be234e97bcd7d6a00b1a6
Jaewon, Jeong <jwjeong12@ezcaretech.com>
2017-12-28 18:18:45 +0900

    Modified

---
+++ Xave/src/web/generator/xave.web.generator.biz/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xave.web.generator.biz
+{
+    public class Class1
+    {
+    }
+}

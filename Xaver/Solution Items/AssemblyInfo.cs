a1b9eb8f8c3ade58ffaf33c96dfe807bca10e6d2
unknown <jwjeong12@hanmail.net>
2018-03-07 22:23:09 +0900

    Modified

--- Xaver/Solution Items/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using System;
-using System.Reflection;
-
-[assembly: AssemblyCopyright("Copyright © Joshua's Xaver. 2017")]
-[assembly: AssemblyInformationalVersion("1.1.1-2017-09-04 09:34:09.245")]
-
-
